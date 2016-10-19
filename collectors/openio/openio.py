4366c030cae22f81e6d01f6c029166fa0f2d1420
vdombrovski <vladimir@openio.io>
2016-10-19 14:36:53 +0200

    Fix proper parsing of filesystem Add config option to specify fs types

--- collectors/openio/openio.py
+++ collectors/openio/openio.py
@@ -16,6 +16,7 @@ Targeted SDS Version: B2 support (unstable)
 import diamond.collector
 import diamond.convertor
 import urllib3
+import shlex
 from oio.common import utils
 import json
 import os
@@ -27,6 +28,7 @@ class OpenIOSDSCollector(diamond.collector.Collector):
     def process_config(self):
         super(OpenIOSDSCollector, self).process_config()
         self.namespaces = self.config['namespaces'] or 'OPENIO'
+        self.fs = self.config['fs-types'] or ('xfs', 'ext4')
 
     def get_default_config_help(self):
         config_help = super(OpenIOSDSCollector, self).get_default_config_help()
@@ -104,23 +106,29 @@ class OpenIOSDSCollector(diamond.collector.Collector):
         """ Fetches the the block id of a provided volume to include
         as an additional tag
         """
-        if volume == '/':
-            return 'rootfs'
         with open("/etc/mtab") as f:
+            rootfs = None
             for line in f:
-                if 'rootfs' in line:
-                    pass
-                else:
-                    l = line.split(' ')
-                    if l[1] in volume:
-                        try:
-                            p = Popen(['blkid', l[0]], stdout=PIPE, stderr=PIPE)
-                            stdout, stderr = p.communicate()
-                            uuid = stdout.split(" ")[1].split("UUID=")[1][:-1]
-                            return str(uuid)
-                        except Exception as e:
-                            self.log.exception(e)
-                            return l[0]
+                l = line.split(' ')
+                if l[1] == "/":
+                    rootfs = line
+                elif any([True for e in self.fs if e in line]):
+                    if volume.startswith(l[1]):
+                        return self.get_blkid(line, volume)
+            if rootfs:
+                return self.get_blkid(rootfs, volume)
+
+    def get_blkid(self, line, volume):
+        try:
+            p = Popen(['blkid', line[0]], stdout=PIPE,
+                      stderr=PIPE)
+            stdout, stderr = p.communicate()
+            params = dict(t.split('=')
+                          for t in shlex.split("VOLUME="+stdout))
+            return str(params['UUID'])
+        except Exception as e:
+            self.log.exception(e)
+            return line[0]
 
     def get_service_diskspace(self, metric_prefix, volume):
         if hasattr(os, 'statvfs'):  # POSIX
@@ -143,7 +151,8 @@ class OpenIOSDSCollector(diamond.collector.Collector):
         dvolume = dvolume.replace('_', '-')
 
         for unit in self.config['byte_unit']:
-            metric_name = '%s.%s.%s_percentfree' % (metric_prefix, dvolume, unit)
+            metric_name = '%s.%s.%s_percentfree'\
+                            % (metric_prefix, dvolume, unit)
             metric_value = float(blocks_free) / float(
                 blocks_free + (blocks_total - blocks_free)) * 100
             self.publish_gauge(metric_name, metric_value, 2)
